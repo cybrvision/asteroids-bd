8402dd5dce0f6da39048c56a1df3850fb4db4caa
7riple7hrea7 <7riple7hrea7@gmail.com>
2025-04-22 22:55:52 -0600

    sprite setup file created

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
+    #
+    ### Variables
+    #
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
+    screen_background = 'black'
 
+    #
+    ### Game Loop
+    #
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill('black')
+        screen.fill(screen_background)
         pygame.display.flip()
-
+        dt = clock.tick(fps)/1000
 
 if __name__ == "__main__":
     main()
